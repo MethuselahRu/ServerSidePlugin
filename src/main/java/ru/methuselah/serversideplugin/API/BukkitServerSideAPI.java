d383d7d4e97598660c0691d4715c1be686815e03
SimSonic <simsonic@yandex.ru>
2015-07-03 12:52:09 +0600

    Версия 0.16a

--- src/main/java/ru/methuselah/serversideplugin/API/BukkitServerSideAPI.java
+++ src/main/java/ru/methuselah/serversideplugin/API/BukkitServerSideAPI.java
@@ -3,6 +3,7 @@ package ru.methuselah.serversideplugin.API;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import ru.methuselah.authlib.links.LinksMethuselah;
 import ru.methuselah.authlib.methods.ResponseException;
 import ru.methuselah.securitylibrary.Data.Methuselah.ProfileInfoPayload;
 import ru.methuselah.securitylibrary.Data.Methuselah.ProfileInfoResponse;
@@ -26,7 +27,7 @@ public final class BukkitServerSideAPI extends ServerSideAPI
 			return cachedInfos.get(uuid);
 		final ProfileInfoPayload payload = new ProfileInfoPayload();
 		payload.uuid = uuid.toString().replace("-", "");
-		final ProfileInfoResponse response = MethuselahPrivate.profileInfo(payload);
+		final ProfileInfoResponse response = new MethuselahPrivate(new LinksMethuselah()).profileInfo(payload);
 		cachedInfos.put(uuid, response);
 		return response;
 	}
